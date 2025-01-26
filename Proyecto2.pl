2211e18586b367858d9dd897aab687a735722b2a
JhonatanHZ <jhonatanhomsany@gmail.com>
2025-01-25 21:24:23 -0400

    Cambios a la 2

--- Proyecto2.pl
+++ Proyecto2.pl
@@ -13,24 +13,19 @@ converter([H|T], [HString|TString]) :- string_chars(HString, H), converter(T, TS
 
 %Parte 2
 
-%Idea de implementacion: Tomamos el primer caracter. Esa sera nuestra primera subcadena. La repetimos
-%y verificamos si coincide con los siguientes caracteres del string original hasta llegar al final.
-%En caso de no coincidir, retrocedemos y añadimos el siguiente caracter del string. 
-%En este punto, repetimos.
-
 ocurrencias(S, Result):- atom_chars(S, Lista), takeSubstrings(Lista, 1, Result).
 
-takeSubstrings(Lista, K, Result):- findSubstrings(Lista, K, Subs), length(Subs, M), length(Lista, N), checkIntegerDivision(M, N, D), repeatSubstring(Subs, D, R), checkEquality(Lista, R), Result = D, K1 is K+1, takeSubstrings(Lista, K1, Result).
+takeSubstrings(Lista, K, Result):- length(Lista, N),
+    (K =< N, findSubstrings(Lista, K, Subs), length(Subs, M), checkIntegerDivision(N, M, D), repeatSubstring(Subs, D, R), checkEquality(Lista, R) -> Result = D;   
+        K1 is K + 1, takeSubstrings(Lista, K1, Result)).
 
-findSubstrings([], _, _) :- !, fail.
 findSubstrings(_, 0, []).
 findSubstrings([X|XS], K, [X|Result]):- K > 0, length([X|XS], N), N >= K, K1 is K - 1, findSubstrings(XS, K1, Result).
 
-checkIntegerDivision(B, C, D):- 0 is B mod C, D is B // C.
+checkIntegerDivision(A, B, D):- 0 is A mod B, D is A // B.
 
 repeatSubstring(_, 0, []).
 repeatSubstring(Subs, N, Result):- N > 0, N1 is N - 1, repeatSubstring(Subs, N1, Temp), append(Subs, Temp, Result).
 
 checkEquality([], []).
 checkEquality([X|XS], [Y|YS]):- X == Y, checkEquality(XS, YS).
-
